8c376ee9268074f5ec5e9ac64c70dc8aa3a6dd69
Matik - PC <mateuszkowalski199700@gmail.com>
2024-02-16 06:57:42 +0100

    Add form validation and password visibility toggle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +21,9 @@ import { HeaderComponent } from './components/header/header.component';
     BrowserModule,
     AppRoutingModule,
     RouterModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -1,26 +1,43 @@
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from './../auth.service';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+	hide: boolean = true;
+  registerForm: FormGroup;
 
   constructor(
-    private authService: AuthService
-  ) { 
-    this.register('test123', 'test123', 'test123@test.test');
-  }
-
-  register(username: string, password: string, email: string) {
-    this.authService.register(username, password, email).subscribe((res) => {
-      console.log(res);
+    private authService: AuthService,
+    private formBuilder: FormBuilder
+  ) {
+    this.registerForm = this.formBuilder.group({
+      username: ['', [Validators.required, Validators.minLength(3)]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      email: ['', [Validators.required, Validators.email]],
     });
   }
 
-  showPassword() {  
-    
+  register() {
+    if (this.registerForm.invalid) {
+      console.log('Invalid form');
+      return;
+    }
+
+    this.authService
+      .register(
+        this.registerForm.value.username,
+        this.registerForm.value.password,
+        this.registerForm.value.email
+      )
+      .subscribe((res) => {
+        console.log(res);
+      });
   }
+
+  showPassword() {}
 }
